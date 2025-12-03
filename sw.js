eab148b894e50929a5d9c6868188f5c3001bab37
gadallaosama03-coder <gadalla.osama03@gmail.com>
2025-12-03 12:35:16 +0100

    initial launch

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'hmi-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './DB.xlsx',
+  './manifest.json',
+  './minilogo.png',
+  './logo.png',
+  './MyA.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })
+    ))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).then((res) => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
